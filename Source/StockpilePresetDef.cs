005e6c9f5617981ef444c5bc911742730ee670de
notfood <notfood@users.noreply.github.com>
2018-11-21 20:21:14 -0500

    Add filter perishables

--- Source/StockpilePresetDef.cs
+++ Source/StockpilePresetDef.cs
@@ -1,18 +1,30 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using RimWorld;
 
 namespace YetAnotherStockpilePresets
 {
     public class StockpilePresetDef : Def
     {
-        public ThingFilter filter;
+        public ThingFilter filter = new ThingFilter();
+        public bool filterPerishables;
 
         public override void ResolveReferences()
         {
-            if (filter == null) {
-                Log.Error("StockpilePresets :: " + defName + " has null filter");
-                return;
-            }
             filter.ResolveReferences();
+            if (filterPerishables) {
+                foreach (var def in filter.AllowedThingDefs.ToList())
+                {
+                    if (def.EverHaulable
+                        && def.CanEverDeteriorate
+                        && def.GetStatValueAbstract(StatDefOf.DeteriorationRate) > 0f) {
+                        continue;
+                    }
+                    filter.SetAllow(def, false);
+                }
+            }
         }
     }
 }
