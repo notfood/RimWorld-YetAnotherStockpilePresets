1c64eae1b56caba1cb28e5f7da750616735951db
notfood <notfood@users.noreply.github.com>
2022-10-25 02:07:49 -0500

    RimWorld 1.4

--- Source/Command_StorageSettingsPresets.cs
+++ Source/Command_StorageSettingsPresets.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using System.Reflection;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -16,16 +16,16 @@ namespace YetAnotherStockpilePresets
             new Harmony("rimworld.notfood.yasp").PatchAll();
         }
 
-        readonly Zone_Stockpile stockpile;
+        readonly IStoreSettingsParent target;
 
-        public Command_StorageSettingsPresets(Zone_Stockpile stockpile)
+        public Command_StorageSettingsPresets(IStoreSettingsParent target)
         {
             icon = StoragePresetsTex;
 
             defaultLabel = ResourceBank.Label;
             defaultDesc = ResourceBank.Description;
 
-            this.stockpile = stockpile;
+            this.target = target;
         }
 
         public override void ProcessInput(Event ev)
@@ -35,23 +35,28 @@ namespace YetAnotherStockpilePresets
             foreach (var def in DefDatabase<StockpilePresetDef>.AllDefs)
             {
                 list.Add(new FloatMenuOption(def.label, delegate {
-                    CopyIntoFilterFromTemplate(stockpile, def);
+                    CopyIntoFilterFromTemplate(target, def);
                 }));
             }
             Find.WindowStack.Add(new FloatMenu(list));
         }
 
-        internal static void CopyIntoFilterFromTemplate(Zone_Stockpile stockpile, StockpilePresetDef def)
+        internal static void CopyIntoFilterFromTemplate(IStoreSettingsParent target, StockpilePresetDef def)
         {
-            stockpile.settings.filter.CopyAllowancesFrom(def.filter);
+            target.GetStoreSettings().filter.CopyAllowancesFrom(def.filter);
         }
     }
 
-    [HarmonyPatch (typeof(Zone_Stockpile))]
-    [HarmonyPatch (nameof(Zone_Stockpile.GetGizmos))]
-    public static class Zone_Stockpile_GetGizmos_Patch
+    [HarmonyPatch]
+    public static class ISelectable_GetGizmos_Patch
     {
-        public static void Postfix(Zone_Stockpile __instance, ref IEnumerable<Gizmo> __result)
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(Zone_Stockpile), nameof(ISelectable.GetGizmos));
+            yield return AccessTools.Method(typeof(Building_Storage), nameof(ISelectable.GetGizmos));
+        }
+
+        public static void Postfix(IStoreSettingsParent __instance, ref IEnumerable<Gizmo> __result)
         {
             __result = __result.AddItem(new Command_StorageSettingsPresets(__instance));
         }
